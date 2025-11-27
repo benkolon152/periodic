42fd65c4869269598f86084858af252f04658952
Benkolon152 <v.benasz@gmail.com>
2025-11-27 11:22:56 +0100

    Read each line

--- src/main/java/com/example/periodic/Elements.java
+++ src/main/java/com/example/periodic/Elements.java
@@ -1,5 +1,7 @@
 package com.example.periodic;
 
+import javafx.scene.paint.Color;
+
 public class Elements {
     public int atomicNumber;
     public String symbol;
@@ -23,7 +25,16 @@ public class Elements {
     groupBlock,
     yearDiscovered */
 
-    public void Element(String csvLine){
-
+    public Elements(String csvLine){
+        String[] split = csvLine.split(",");
+        //System.out.println(split);
+        atomicNumber = Integer.parseInt(split[0]);
+        symbol = split[1].trim();
+        name = split[2].trim();
+        atomicMass = split[3].trim();
+        cpkHexColor = split[4];
+        Color cpkColor = Color.TRANSPARENT;
+        if (cpkHexColor.length() == 6)
+             cpkColor = Color.web("#"+cpkHexColor);
     }
 }
--- src/main/java/com/example/periodic/PeriodicTableController.java
+++ src/main/java/com/example/periodic/PeriodicTableController.java
@@ -5,19 +5,37 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 import javax.swing.text.html.parser.Element;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class PeriodicTableController implements Initializable {
-    public List<Element> elements;
+    public List<Elements> elements;
 
-    public void readCsv(String filename){
-
+    public void readCsv(String filename) throws FileNotFoundException {
+        elements = new ArrayList<>();
+        File fin = new File(filename);
+        Scanner sfin = new Scanner(fin);
+        String firstLine = sfin.nextLine();
+        while (sfin.hasNextLine()){
+            String fileLine = sfin.nextLine();
+            elements.add(new Elements(fileLine));
+        }
+        sfin.close();
+        System.out.println(elements.size());
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        readCsv("periodic-table-data.csv");
+        try {
+            readCsv("periodic-table-data.csv");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
