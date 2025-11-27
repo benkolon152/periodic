7249f8a036c9bfe1f8c1439fc516723a3a8af632
Benkolon152 <v.benasz@gmail.com>
2025-11-27 10:45:30 +0100

    stuff

--- src/main/java/com/example/periodic/PeriodicTableApplication.java
+++ src/main/java/com/example/periodic/PeriodicTableApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class PeriodicTableApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(PeriodicTableApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(PeriodicTableApplication.class.getResource("PeriodicTable-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("Periodic table!");
         stage.setScene(scene);
         stage.show();
     }
